560c434652e8d27a5f26d4f6a3f9c56a80cdd673
Ola Thoresen <ola@nytt.no>
2022-12-03 19:31:39 +0000

    Add sw_version to diagnostics

--- homeassistant/components/twinkly/light.py
+++ homeassistant/components/twinkly/light.py
@@ -23,13 +23,15 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_SW_VERSION, CONF_MODEL
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers import device_registry as dr
+# from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import (
     ATTR_VERSION,
     CONF_HOST,
     CONF_ID,
-    CONF_NAME,
+    ONF_NAME,
     DATA_CLIENT,
     DATA_DEVICE_INFO,
     DEV_LED_PROFILE,
@@ -175,6 +177,13 @@ class TwinklyLight(LightEntity):
             self._attr_supported_features = (
                 self.supported_features & ~LightEntityFeature.EFFECT
             )
+
+            device_registry = dr.async_get(self.hass)
+            device_entry = device_registry.async_get_device({(DOMAIN, self._id)}, set())
+            if device_entry:
+                device_registry.async_update_device(
+                    device_entry.id, sw_version=self._software_version
+                )
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn device on."""
